19c3d44168f36c2270e470c2db2b241f82751fe6
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-31 18:38:53 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+def reverse_each_word(sentence1,sentence2=nil)
+if sentence2
+  sent2 = sentence2
+  i=0
+else
+  sent2=sentence1.split
+sent2.collect { |e|e.reverse }
+.join(" ")
+end
+
+end
+# sentence1.each{|e|p e.reverse}
+#.each{ |pair| p pair }
+
+
+
+# new_array = []
+#
+# sentence1.each{|e|p e}
